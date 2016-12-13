2bffeb34d43d0c611d4917a8e160e5d1c94bcb7c
8Macosta <8racosta@stpatrick.org>
2016-12-13 10:54:02 -0600

    Add files via upload

---
+++ item.js
@@ -0,0 +1,15 @@
+class Item{
+  //Fill in the item class below this comment.
+constructor(n,p,st){
+  this.name = n;
+  this.price = p;
+  this.shippingTime = st;
+}
+
+
+}
+
+//Create your three test items below this comment.
+let mygift = new Item("HDTV",260,6)
+let yourgift = new Item("Samsung",600,7)
+let hisgift = new Item("Ps4",900,9)
