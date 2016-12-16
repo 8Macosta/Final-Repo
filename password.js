e5b6467f12de31466294f2d6119a3c03b77ed1db
8Macosta <8racosta@stpatrick.org>
2016-12-16 10:52:28 -0600

    Add files via upload

--- password.js
+++ password.js
@@ -1,11 +1,27 @@
 class Password{
   //The first part of the class.
-
-
-
-  //Instance functions below this comment.
-
-
-
-  //Static function below this comment.
+constructor(){
+  this.publickey = publicky;
+  this.privatekey = privatekey;
 }
+validPublicKey(){
+  let key = this.publicky;
+  if(key.length >= 8 && key.length <= 25){
+    return true;
+  }
+  else{
+    return false;
+  }
+}
+   validPrivatekey(){
+     let pkey = this.privatekey;
+     if(pkey.)
+   }
+   static makePrivatekey(){
+     let key = '';
+     let limit = 14;
+     for(x=0;x<5;x++){
+
+     }
+   }
+ }
